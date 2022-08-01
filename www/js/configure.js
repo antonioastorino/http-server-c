a6d5c6b31392d1e8ebb534b33fe9b1973cf7a20d
Antonio Astorino <antonio.astorino.phd@gmail.com>
2022-08-01 09:09:27 +0200

    Working on Configuration

--- www/js/configure.js
+++ www/js/configure.js
@@ -1,15 +1,10 @@
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+function labelTextUpdate(ev) {
+  let value = ev.target.value;
+  let id = ev.target.id;
+  let split_text = value.split(",").map((elem) => {
+    return elem.trim();
+  });
+  console.log(split_text);
 }
 
 function generateInput() {
@@ -21,6 +16,7 @@ function generateInput() {
     new_input.setAttribute("id", `input_${i}`);
     new_input.setAttribute("name", `input_${i}`);
     new_input.setAttribute("value", `value ${i}`);
+    new_input.addEventListener("change", labelTextUpdate);
     new_label.setAttribute("for", `input_${i}`);
     new_label.innerText = `Label ${i}: `;
     new_div.appendChild(new_label);
@@ -29,6 +25,23 @@ function generateInput() {
   }
 }
 
+function initCanvas() {
+  let canvas = document.getElementById("canvas");
+  const v_space = 100;
+  const h_space = 20;
+  const line_length = 100;
+  let ctx = canvas.getContext("2d");
+  for (let i = 0; i < 4; i++) {
+    ctx.moveTo(0, 10 + i * v_space);
+    ctx.lineTo(line_length, 10 + i * v_space);
+    ctx.stroke();
+    ctx.moveTo(line_length + h_space, 10 + i * v_space);
+    ctx.lineTo(2 * line_length + h_space, 10 + i * v_space);
+    ctx.stroke();
+  }
+}
+
 window.onload = function () {
   generateInput();
+  initCanvas();
 };
