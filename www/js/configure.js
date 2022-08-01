86ede49fe10abeddce915dcf2438b3a53baff543
Antonio Astorino <antonio.astorino.phd@gmail.com>
2022-08-01 11:52:50 +0200

    Refactored line creation

--- www/js/configure.js
+++ www/js/configure.js
@@ -1,3 +1,24 @@
+var lines = [];
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Line {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+  draw(ctx) {
+    ctx.moveTo(this.start.x, this.start.y);
+    ctx.lineTo(this.end.x, this.end.y);
+    ctx.stroke();
+  }
+}
+
 function labelTextUpdate(ev) {
   let value = ev.target.value;
   let id = ev.target.id;
@@ -5,6 +26,7 @@ function labelTextUpdate(ev) {
     return elem.trim();
   });
   console.log(split_text);
+  updateCanvas();
 }
 
 function generateInput() {
@@ -27,21 +49,43 @@ function generateInput() {
 
 function initCanvas() {
   let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  lines.forEach((elem) => {
+    elem.draw(ctx);
+  });
+}
+
+function updateCanvas() {
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  initCanvas();
+}
+
+function initLines(number_of_lines) {
   const v_space = 100;
   const h_space = 20;
-  const line_length = 100;
-  let ctx = canvas.getContext("2d");
-  for (let i = 0; i < 4; i++) {
-    ctx.moveTo(0, 10 + i * v_space);
-    ctx.lineTo(line_length, 10 + i * v_space);
-    ctx.stroke();
-    ctx.moveTo(line_length + h_space, 10 + i * v_space);
-    ctx.lineTo(2 * line_length + h_space, 10 + i * v_space);
-    ctx.stroke();
+  const left_line_length = 100;
+  const right_line_length = 200;
+  const top_margin = 10;
+  const left_line_left_margin = 10;
+  const right_line_left_margin = left_line_left_margin + left_line_length + h_space;
+  for (let line_nr = 0; line_nr < number_of_lines; line_nr++) {
+    new_left_line = new Line(
+      new Point(left_line_left_margin, top_margin + line_nr * v_space),
+      new Point(left_line_left_margin + left_line_length, top_margin + line_nr * v_space)
+    );
+    lines.push(new_left_line);
+    new_right_line = new Line(
+      new Point(right_line_left_margin, top_margin + line_nr * v_space),
+      new Point(right_line_left_margin + right_line_length, top_margin + line_nr * v_space)
+    );
+    lines.push(new_right_line);
   }
 }
 
 window.onload = function () {
+  initLines(4);
   generateInput();
   initCanvas();
 };
